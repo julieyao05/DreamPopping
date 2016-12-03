fcfe3e7e15fee1107ee6165a30161505fef88527
Sonal Prasad <sonalprasad.sp@gmail.com>
2016-12-03 15:07:17 -0800

    Added Overworld's global score

---
+++ Assets/OverworldScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverworldScore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//GlobalScore.add(10);
+}
